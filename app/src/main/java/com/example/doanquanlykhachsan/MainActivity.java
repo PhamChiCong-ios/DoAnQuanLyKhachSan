87f713fbca5bf3942a63796ff4372a4b72b3c85d
PhamChiCong-ios <impero233@gmail.com>
2021-10-22 08:15:18 +0700

    Initial commit

---
+++ app/src/main/java/com/example/doanquanlykhachsan/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.doanquanlykhachsan;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    int i;
+    public static String key = "";
+//dheflkewhofoew
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.ad_xem_dich_vu);
+         startActivity(new Intent(getApplicationContext(),sign_in.class));
+//        i = 0;
+//        StaticConfig.mRoom.addValueEventListener(new ValueEventListener() {
+//            @Override
+//            public void onDataChange(@NonNull DataSnapshot snapshot) {
+//                for (DataSnapshot ds : snapshot.getChildren()) {
+//                    if (ds.exists()) {
+//                        i++;
+//                        key = "L0" + i;
+//                    }
+//
+//                }
+//
+//            }
+//
+//            @Override
+//            public void onCancelled(@NonNull DatabaseError error) {
+//
+//            }
+//        });
+//        Room room = new Room(key, "phog1", "", "", 0, 0);
+//        StaticConfig.mRoom.child(key).setValue(room);
+//        Log.e("test", StaticConfig.roomkey);
+    }
+}
